9bf43d3c0dc40d3d022bcf7a36623e53a1a4f202
mugt <nwck.thomas@gmail.com>
2016-12-17 13:30:43 +0100

    Cration donné Product et ProductItem

--- backoffice/models.py
+++ backoffice/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    """
+    Documentation Product Class
+    - name CharField(max_length=100)
+    - code IntegerField()
+    """
+    name = models.CharField(max_length=100)
+    code = models.IntegerField()
+
+    def __unicode__(self):
+        return "{0} [{1}]".format(self.name, self.code)
+
+
+class ProductItem(models.Model):
+    color   = models.CharField(max_length=100)
+    product = models.ForeignKey('Product')
+    code    = models.IntegerField()
+
+    def __unicode__(self):
+        return "{0} {{1}} [{2}]".format(self.product.name, self.color, self.code)
